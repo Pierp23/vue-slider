248b7863b3b79cfd9721abe5c795ba33d96214d5
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-07-03 21:04:24 +0200

    ADD infinite click

--- js/script.js
+++ js/script.js
@@ -6,31 +6,48 @@ createApp({
             slides: [
             {
                 image: 'img/01.webp',
-                title: 'Marvel\'s Spiderman Miles Morale',
+                title: 'Marvel\'s Spiderman Miles Morales',
                 text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                display: true
             }, {
                 image: 'img/02.webp',
                 title: 'Ratchet & Clank: Rift Apart',
                 text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                display: false
             }, {
                 image: 'img/03.webp',
                 title: 'Fortnite',
                 text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                display: false
             }, {
                 image: 'img/04.webp',
                 title: 'Stray',
                 text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                display: false
             }, {
                 image: 'img/05.webp',
                 title: "Marvel's Avengers",
                 text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                display: false
             }
-        ]}
+        ],
+        indexActive: 0}
+    },
+    methods: {
+        prevClick(indice){
+            if(this.indexActive > 0){
+                this.indexActive--;
+            }
+            else if (this.indexActive == 0){
+                this.indexActive = this.slides.length - 1;
+           }
+           
+
+        },
+        nextClick(indice){
+            if(this.indexActive < this.slides.length - 1){
+                this.indexActive++;
+            }
+            else if (this.indexActive == this.slides.length - 1){
+                this.indexActive = 0;
+            }
+            
+        }
     }
 }).mount('#app');
 
